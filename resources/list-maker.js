30b1d29e52e3c4b733fc52633964f9c10b0b4f44
mel <43204513+melibooo@users.noreply.github.com>
2020-11-22 13:58:33 -0800

    Create HTML template for making lists using Javascript, rather than manually programming new HTML for every list I want to make - first working version

--- resources/list-maker.js
+++ resources/list-maker.js
@@ -9,18 +9,24 @@ const skiTouringRadioTarget = $('ski-touring-radio');
 const mountainBikingRadioTarget = $('mountain-biking-radio');
 const dayOrOvernightTarget = $('day-or-overnight-selection');
 
-const skiTouringGearTarget = $('ski-touring-gear');
-const skiTouringClothingTarget = $('ski-touring-clothing');
+const skiTouringGearTarget = $('list-ski-gear');
+const skiTouringClothingTarget = $('list-ski-clothing');
 
-const itemIds = ['rescue', 'skis-snowboard', 'skins', 'poles', 'boots', 'helmet', 'goggles', 'sunglasses'];
-const deleteButtonTargets = itemIds.map(itemId => $(`delete-${itemId}-ski-gear`));
-const listItemTargets = itemIds.map(itemId => $(`item-${itemId}-ski-gear`));
-const associatedHrTargets = itemIds.map(itemId => $(`hr-${itemId}-ski-gear`));
+//const deleteButtonTargets = itemIds.map(itemId => $(`delete-${itemId}-ski-gear`));
+//const listItemTargets = itemIds.map(itemId => $(`item-${itemId}-ski-gear`));
+//const associatedHrTargets = itemIds.map(itemId => $(`hr-${itemId}-ski-gear`));
 
 const userInputTextTarget = $('user-input-text');
 const addListItemTarget = $('add-list-item');
 const newListItemsSkiGearTarget = $('new-list-items-ski-gear');
 const newListItemsSkiClothingTarget = $('new-list-items-ski-clothing');
+
+// Objects containing information for the lists
+const skiGearList = {
+  activity:'ski',
+  listTitle: 'Gear',
+  itemDisplayNames: ['Transceiver, probe and shovel', 'Touring skis/splitboard', 'Skins', 'Poles', 'Boots', 'Helmet', 'Goggles', 'Sunglasses']
+}
 
 // Functions to customise the DOM
 
@@ -40,6 +46,60 @@ function displaySkiListPanels() {
 function hideSkiGearListPanel() {
   skiTouringGearTarget.style.display = 'none';
   skiTouringClothingTarget.style.display = 'none';
+}
+
+// Function to delete an item
+function deleteListItem(itemId, activity, listTitleId) {
+  const listItemTarget = $(`container-${itemId}-${activity}-${listTitleId}`);
+  listItemTarget.remove();
+  const associatedHrTargets = $(`hr-${itemId}-${activity}-${listTitleId}`)
+  associatedHrTargets.remove();
+}
+
+// Function to create event listeners of the delete buttons
+const createDeleteButtonListeners = (itemIds, activity, listTitleId) => {
+  itemIds.forEach(itemId => {
+    const deleteButtonTarget = $(`delete-${itemId}-${activity}-${listTitleId}`);
+    deleteButtonTarget.addEventListener('click', function() {
+      deleteListItem(itemId, activity, listTitleId);
+    });
+  });
+};
+
+// Function to render lists as HTML
+function renderLists(activity, listTitle, itemDisplayNames) {
+  console.log('renderLists function successfully called');
+  
+  // Create ids from the list title and the list item display names
+  const listTitleId = listTitle.toLowerCase();
+  const itemIds = itemDisplayNames.map(itemDisplayName => itemDisplayName.toLowerCase().replace(/\s+|[,\/]/g, '-'));
+
+  // Generate the html to render the list
+  const startListHtml = `<section class="list-container" id="list-${activity}-${listTitleId}">
+  <h3>${listTitle}</h3>`;
+  const endListHtml = '</section>';
+  
+  let listItemsHtml = '';
+  for (let i=0; i<itemDisplayNames.length; i++) {
+    listItemsHtml += `
+    <div class="list-item" id="container-${itemIds[i]}-${activity}-${listTitleId}">
+      <button id="delete-${itemIds[i]}-${activity}-${listTitleId}">x</button>
+      <label for="item-${itemIds[i]}-${activity}-${listTitleId}">${itemDisplayNames[i]}</label>
+      <input type="checkbox" id="item-${itemIds[i]}-${activity}-${listTitleId}" name="item-${activity}-${listTitleId}" checked>
+    </div>
+    <hr id="hr-${itemIds[i]}-${activity}-${listTitleId}"></hr>`;
+  }
+
+  const userListItemsHtml = `<div id="new-list-items-ski-gear"></div>
+  <div class="text-input-line">
+    <label for="user-input-text"><input type="text" name="ski-touring-list-item" id="user-input-text"></label>
+    <button id="add-list-item">Add</button>
+  </div>`;
+  
+  // Render the html on the webpage
+  const listsTarget = $('lists-container');
+  listsTarget.innerHTML = startListHtml + listItemsHtml + userListItemsHtml + endListHtml;
+  
 }
 
 // Function to delete an item
@@ -94,24 +154,27 @@ function addItemToList() {
   userInputTextTarget.value = '';
 }
 
+
+
 // Event listeners
 
 // Depending on whether the user selects 'ski touring' or 'mountain biking', the relevant lists will appear, and the irrelevant lists will be disappear
 skiTouringRadioTarget.addEventListener('change', function() {
-  displaySkiListPanels();
+  // Website renderers
+  renderLists(skiGearList.activity, skiGearList.listTitle, skiGearList.itemDisplayNames);
   displayDayOrOvernightQuestion();
 });
 mountainBikingRadioTarget.addEventListener('change', function() {
-  hideSkiGearListPanel();
+  //hideSkiGearListPanel();
   hideDayOrOvernightQuestion();
 });
 
-deleteButtonTargets.forEach(deleteButtonTarget => {
+/*deleteButtonTargets.forEach(deleteButtonTarget => {
   const index = deleteButtonTargets.indexOf(deleteButtonTarget);
   deleteButtonTarget.addEventListener('click', function() {
     deleteItem(index)
   });
-});
+});*/
 
 
 // When the user clicks the "add" button or hits the enter key, whatever the user has typed into the text input box will be added to the list as an item
