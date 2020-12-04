62343176c07e32154805ac318216a5b337c8e9d4
mel <43204513+melibooo@users.noreply.github.com>
2020-12-03 16:27:42 -0800

    Test the 'send-message.php' file

--- resources/php/send-message.php
+++ resources/php/send-message.php
@@ -1,8 +1,1 @@
-This function isn't working yet, I'm sorry!
-
-<?php
-	$to = "mellim.dev@gmail.com";
-	$subject = "Email from my website";
-	$body = "the user typed in ...";
-	mail($to, $subject, $body);
-?>
+You typed in: <?php print $_POST['myEmail'];?>
